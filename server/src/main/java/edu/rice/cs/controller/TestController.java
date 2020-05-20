dd764bdbaccabb9769194d8e265c21ae560368ba
joeyhaohao <johnzyh9669@gmail.com>
2020-05-20 00:16:12 -0500

    add server

---
+++ server/src/main/java/edu/rice/cs/controller/TestController.java
@@ -0,0 +1,15 @@
+package edu.rice.cs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="joey", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ server/src/main/webapp/WEB-INF/static/greeting.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: joeyhaohao
+  Date: 5/19/20
+  Time: 9:22 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE HTML>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <title>Getting Started: Serving Web Content</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+    <p th:text="'Hello, ' + ${name} + '!'" />
+</body>
+</html>
