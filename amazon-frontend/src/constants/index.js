26bc341d6c28bee855f42ae5fa6d2515ab22ebfc
joeyhaohao <johnzyh9669@gmail.com>
2020-07-11 11:48:19 -0500

    enable realtime recommendation

--- amazon-frontend/src/constants/index.js
+++ amazon-frontend/src/constants/index.js
@@ -1,5 +1,7 @@
+// export const API_BASE_URL = 'http://localhost:8080/api';
 export const API_BASE_URL = 'http://ec2-18-217-80-11.us-east-2.compute.amazonaws.com:8080/api';
 
+// export const BASE_URL = 'http://localhost:8080';
 export const BASE_URL = 'http://ec2-18-217-80-11.us-east-2.compute.amazonaws.com:8080';
 
 export const ACCESS_TOKEN = 'accessToken';
--- run_aws.sh
+++ run_aws.sh
@@ -9,4 +9,5 @@ nohup redis-server > logs/redis-logs &
 nohup /home/ubuntu/kafka_2.11-2.4.1/bin/zookeeper-server-start.sh kafka_2.11-2.4.1/config/zookeeper.properties > logs/zookeeper-logs &
 nohup /home/ubuntu/kafka_2.11-2.4.1/bin/kafka-server-start.sh kafka_2.11-2.4.1/config/server.properties > logs/kafka-logs &
 nohup java -jar springbackend/target/springbackend-0.0.1-SNAPSHOT.jar > logs/springbackend-logs &
+nohup java -jar recommender/target/realtime-recommender-jar-with-dependencies.jar > logs/recommender-logs &
 cd amazon-frontend && npm run build && serve build -p 3000 &
