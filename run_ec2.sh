34d64bf4ec7dc90137c93267c2a53d517e358470
joeyhaohao <johnzyh9669@gmail.com>
2020-09-06 22:21:11 -0500

    Add snapshot

--- run_ec2.sh
+++ run_ec2.sh
@@ -6,6 +6,7 @@ fi
 
 kill $(sudo lsof -t -i:8080) # kill spring backend
 kill $(sudo lsof -t -i:3000) # kill react frontend
+kill $(sudo lsof -t -i:7878) # kill recommender server
 sudo /etc/init.d/redis-server stop # stop redis
 sudo /usr/local/kafka_2.11-2.4.1/bin/zookeeper-server-stop.sh # stop zookeeper
 sudo /usr/local/kafka_2.11-2.4.1/bin/kafka-server-stop.sh # stop kafka
@@ -16,3 +17,4 @@ nohup java -Xmx128M -Xms128M -jar springbackend/target/springbackend-0.0.1-SNAPS
 sudo /etc/init.d/redis-server start > logs/redis-logs &
 nohup /usr/local/kafka_2.11-2.4.1/bin/zookeeper-server-start.sh /usr/local/kafka_2.11-2.4.1/config/zookeeper.properties > logs/zookeeper-logs &
 nohup /usr/local/kafka_2.11-2.4.1/bin/kafka-server-start.sh /usr/local/kafka_2.11-2.4.1/config/server.properties > logs/kafka-logs &
+nohup java -Xmx512m -Xms512m -Dserver.port=7878 -jar recommender/target/realtime-recommender-jar-with-dependencies.jar > logs/recommender-logs &
--- run_local.sh
+++ run_local.sh
@@ -9,5 +9,3 @@ nohup redis-server /usr/local/etc/redis.conf > logs/redis-logs &
 nohup zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties > logs/zookeeper-logs &
 nohup kafka-server-start /usr/local/etc/kafka/server.properties > logs/kafka-logs &
 elasticsearch > logs/elasticsearch-logs &
-#nohup /usr/local/Cellar/kafka/2.5.0/bin/zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties > logs/zookeeper-logs &
-#nohup /usr/local/Cellar/kafka/2.5.0/bin/kafka-server-start /usr/local/Cellar/kafka/2.5.0/libexec/config/server.properties > logs/kafka-logs &
